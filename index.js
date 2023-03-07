656f9fe78cc2b5424812cfea69d07270466a9cba
Tayler Schanks <taylerschanks2@gmail.com>
2023-03-07 13:48:06 -0600

    commit tree 49c049baf95ef282bbd86133713fefdea31ea003

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Tayler",
+    streetAddress: "Shelbyville",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const employeeTwo = { ...obj };
+
+    employeeTwo[key] = value;
+
+    return employeeTwo;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const employeeThree = { ...key };
+
+    employeeThree[key];
+
+    return employeeThree;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete employee[key];
+
+    return employee;
+}
